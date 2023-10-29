06c82d6fe737a8b87a06b95c2cbacab6a1940837
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-10-29 14:02:13 -0500

    Text skips properly~!

--- dialog_runner/states/active_dialog_text.gd
+++ dialog_runner/states/active_dialog_text.gd
@@ -11,17 +11,26 @@ var text_data: DialogTextNodeData:
 var index: int
 
 #
+#	Private Variables
+#
+
+var _skip_instead_of_progress: bool
+
+#
 #	State Functions
 #
 
 func state_enter(_message: Dictionary = {}) -> void:
 	_get_parent_state().state_enter(_message)
 	runner.dialog_interacted.connect(_on_dialog_interacted.bind())
+	text_window.state_changed.connect(_on_text_window_state_changed.bind())
 	
 	index = -1
+	_skip_instead_of_progress = false
 	_display_next_text()
 	
 func state_exit() -> void:
+	text_window.state_changed.disconnect(_on_text_window_state_changed.bind())
 	runner.dialog_interacted.disconnect(_on_dialog_interacted.bind())	
 	_get_parent_state().state_exit()
 
@@ -30,6 +39,12 @@ func state_exit() -> void:
 #
 
 func _display_next_text() -> void:
+	# Fast forward instead of progressing to the next text in the dialog, if
+	# necessary.
+	if _skip_instead_of_progress:
+		text_window.fast_forward()
+		return
+	
 	index += 1
 	
 	# If we're out of text for this node, move on
@@ -53,3 +68,10 @@ func _go_to_next_node() -> void:
 
 func _on_dialog_interacted() -> void:
 	_display_next_text()
+
+## WHenever the text window state changes, check to see if the window is
+## showing text. If it is, we'll fast-forward the text instead of just
+## jumping to the next dialog.
+func _on_text_window_state_changed(_state: TextWindowState, path: String) -> void:
+	_skip_instead_of_progress = path.ends_with("Showing")
+	print(path)

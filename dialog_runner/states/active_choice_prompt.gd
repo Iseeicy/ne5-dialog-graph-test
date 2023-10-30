c7a6b6c3fb91c4addde965f32decb775dd44cbfa
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-10-30 11:49:42 -0500

    Started choice prompt handler

--- dialog_runner/states/active_choice_prompt.gd
+++ dialog_runner/states/active_choice_prompt.gd
@@ -1,2 +1,27 @@
 extends DialogRunnerActiveHandlerState
 
+#
+#	Public Variables
+#
+
+var choice_data: ChoicePromptNodeData:
+	get:
+		return data as ChoicePromptNodeData
+
+#
+#	State Functions
+#
+
+func state_enter(_message: Dictionary = {}) -> void:
+	_get_parent_state().state_enter(_message)
+	
+	# Construct the prompt to display in the text window
+	var prompt = TextWindowChoicePrompt.create_prompt_with_text(
+		choice_data.text, 
+		choice_data.choices
+	)
+	
+	text_window.show_choice_prompt(prompt)
+	
+func state_exit() -> void:
+	_get_parent_state().state_exit()

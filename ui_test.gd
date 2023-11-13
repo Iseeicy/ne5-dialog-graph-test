7e17957c5bf91d667096d5a3d02105fa3dece9ac
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-11-13 15:01:57 -0600

    Refactored entire graph setup

--- ui_test.gd
+++ ui_test.gd
@@ -1,11 +1,11 @@
 extends Control
 
 func _ready():
-	$DialogGraphEditor.load_from_resource(DialogGraph.new())
+	$DialogGraphEditor.load_from_resource(PackedDialogGraph.new())
 
 
 func _on_save_button_pressed():
-	var new_graph = DialogGraph.new()
+	var new_graph = PackedDialogGraph.new()
 	$DialogGraphEditor.save_to_resource(new_graph)
 	ResourceSaver.save(new_graph, "res://test_graph.tres")
 	
